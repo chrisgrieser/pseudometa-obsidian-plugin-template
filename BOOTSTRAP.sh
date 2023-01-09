6cf957f54e23dc6bdaefa744254bd58d7fdc3bd9
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-01-09 14:36:41 +0100

    fix

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -1,6 +1,6 @@
 echo "Plugin Name:"
 read -r name
-
+echo
 echo "Plugin Description:"
 read -r desc
 
@@ -8,18 +8,23 @@ read -r desc
 id=$(git remote -v | head -n1 | sed 's/.*\///' | sed 's/\.git.*//')
 
 # plugin class can be id in camelcase and therefore also inferred
-class=$(echo "$id" | perl -pe 's/(^|-)(\w)/\U$2/g')
+class=$(echo "$id" | perl -pe 's/-(\w)/\U$1/g')
+
+year=$(date +"%Y")
 
 #───────────────────────────────────────────────────────────────────────────────
 
 # replace them all
 # $1: placeholder name as {{mustache-template}}
 # $2: the replacement
-function replacePlaceholders () {
-	find . -type f -exec -not -path '*/\.git/' sed -i '' "s/\{\{$1\}\}/$2/g" {} +
+function replacePlaceholders() {
+	# INFO macOS' sed requires sed i '', remove the '' when on Linux or using GNU sed
+	LC_ALL=C # prevent byte sequence error for whatever reason
+	find . -type f -not -path '*/\.git/*' -not -name ".DS_Store" -not -path '*/node_modules/*' -exec sed -i '' "s/{{$1}}/$2/g" {} \;
 }
 
 replacePlaceholders "plugin-name" "$name"
 replacePlaceholders "plugin-id" "$id"
 replacePlaceholders "plugin-desc" "$desc"
 replacePlaceholders "plugin-class" "$class"
+replacePlaceholders "year" "$year"
