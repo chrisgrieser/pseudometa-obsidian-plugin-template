ab8cba8cef41e9b5028b50789ee99fa8e185e1d3
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-01-09 14:54:38 +0100

    feat: infers pugin description from github

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -1,15 +1,19 @@
+#!/usr/bin/env zsh
+
 echo "Plugin Name:"
 read -r name
-echo
-echo "Plugin Description:"
-read -r desc
 
 # plugin id is the same as the git repo name and can therefore be inferred
-id=$(git remote -v | head -n1 | sed 's/.*\///' | sed 's/\.git.*//')
+repo=$(git remote -v | head -n1 | sed 's/\.git.*//' | sed 's/.*://')
+id=$(echo "$repo" | cut d/ -f2)
+
+# desc can be inferred from github description (not using jq for portability)
+desc=$(curl -sL https://api.github.com/repos/chrisgrieser/pseudometa-obsidian-plugin-template | grep "description" | head -n1 | cut -d'"' -f4)
 
 # plugin class can be id in camelcase and therefore also inferred
 class=$(echo "$id" | perl -pe 's/-(\w)/\U$1/g')
 
+# current year for license
 year=$(date +"%Y")
 
 #───────────────────────────────────────────────────────────────────────────────
