f593621fcaf0469bf011227b963c259a45cc516c
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-08-13 00:11:14 +0200

    chore

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -12,10 +12,8 @@ const prod = process.argv[2] === "production";
 
 esbuild
 	.build({
-		banner: {
-			js: banner,
-		},
-		entryPoints: ["main.ts"],
+		banner: { js: banner },
+		entryPoints: ["src/main.ts"],
 		bundle: true,
 		external: ["obsidian", "electron", ...builtins],
 		format: "cjs",
