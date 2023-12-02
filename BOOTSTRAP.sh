184b12d32d521f1e3d32f426ae4ac9cb3b992a1a
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-12-02 09:42:46 +0100

    chore: add support for `HelpMate`

--- BOOTSTRAP.sh
+++ BOOTSTRAP.sh
@@ -5,9 +5,10 @@ set -e # abort when any command errors, prevents this script from self-removing 
 echo -n "Plugin Name:"
 read -r name
 
-# plugin id is the same as the git repo name and can therefore be inferred
 repo=$(git remote -v | head -n1 | sed 's/\.git.*//' | sed 's/.*://')
-# "The id can't contain `obsidian`." https://docs.obsidian.md/Plugins/Releasing/Submit+your+plugin#Step+3+Submit+your+plugin+for+review
+
+# plugin id is the same as the git repo name and can therefore be inferred
+# INFO "The id can't contain `obsidian`." https://docs.obsidian.md/Plugins/Releasing/Submit+your+plugin#Step+3+Submit+your+plugin+for+review
 id=$(echo "$repo" | cut -d/ -f2 | sed -E 's/-?obsidian-?//')
 
 # desc can be inferred from github description (not using jq for portability)
@@ -21,17 +22,19 @@ year=$(date +"%Y")
 
 #───────────────────────────────────────────────────────────────────────────────
 
+LC_ALL=C # prevent byte sequence error
+
 # replace them all
 # $1: placeholder name as {{mustache-template}}
 # $2: the replacement
 function replacePlaceholders() {
 	# INFO macOS' sed requires `sed -i ''`, remove the `''` when on Linux or using GNU sed
-	LC_ALL=C # prevent byte sequence error
 	find . -type f -not -path '*/\.git/*' -not -name ".DS_Store" -not -path '*/node_modules/*' -exec sed -i '' "s/$1/$2/g" {} \;
 }
 
 replacePlaceholders "{{plugin-name}}" "$name"
 replacePlaceholders "{{plugin-id}}" "$id"
+replacePlaceholders "{{plugin-repo}}" "$repo"
 replacePlaceholders "{{plugin-desc}}" "$desc"
 replacePlaceholders "{{plugin-class}}" "$class"
 replacePlaceholders "{{year}}" "$year"
