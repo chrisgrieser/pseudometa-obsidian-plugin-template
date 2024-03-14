b4db7401213e473e2e69e0445b220645d87653c2
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-14 16:13:10 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "obsidian";
 
 export default class {{plugin-class}} extends Plugin {
+	cssclass = this.manifest.id;
 
 	override onload() {
 		console.info(this.manifest.name + " Plugin loaded.");
